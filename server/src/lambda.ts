256b6285654d2447a0c5b396d149743a6179c581
Fernando Ibañez <fernandoibanez@Fernandos-MacBook-Pro.local>
2025-09-17 11:38:21 -0300

    Replacing App Runner with Api Gateway + Lambda - update lambda.ts to use ExpressAdapter

--- server/src/lambda.ts
+++ server/src/lambda.ts
@@ -2,22 +2,22 @@ import { configure as serverlessExpress } from '@codegenie/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import type { APIGatewayProxyEvent, Context, Handler } from 'aws-lambda';
+import type { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-let cachedServer: Handler;
+let cachedServer: ReturnType<typeof serverlessExpress>;
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<unknown> => {
   if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
+    const expressApp = express();
+    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
     nestApp.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await nestApp.init();
-    // The serverlessExpress library does not have strong typing for its return value.
-    // We are disabling this rule for this line as we know the assignment is safe.
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
+      app: expressApp,
     });
   }
 
-  return cachedServer(event, context, () => null);
+  return cachedServer(event, context, () => {});
 };
